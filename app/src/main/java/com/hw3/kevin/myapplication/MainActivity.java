bf37eb8205cd28d0aa5f6a118f7b74b98699df38
Kevin Chung <kvnchung92@gmail.com>
2015-11-13 18:38:49 -0600

    initial commit

---
+++ app/src/main/java/com/hw3/kevin/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hw3.kevin.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

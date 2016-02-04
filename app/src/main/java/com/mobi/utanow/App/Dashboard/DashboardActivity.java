68b6a2027515aa159d7310e03fc708b13dcf3839
Kevin Chung <kvnchung92@gmail.com>
2016-02-04 11:21:01 -0600

    added facebook login functionality

--- app/src/main/java/com/mobi/utanow/App/Dashboard/DashboardActivity.java
+++ app/src/main/java/com/mobi/utanow/App/Dashboard/DashboardActivity.java
@@ -1,7 +1,40 @@
 package com.mobi.utanow.App.Dashboard;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.login.LoginManager;
+import com.mobi.utanow.App.Login.LoginActivity;
+import com.mobi.utanow.myapplication.R;
+
 /**
  * Created by Anthony on 11/13/15.
  */
-public class DashboardActivity {
+public class DashboardActivity extends AppCompatActivity{
+
+    Intent intent;
+    Context context;
+    Button logoutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        context = this;
+
+        logoutButton = (Button)findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //logout
+                LoginManager.getInstance().logOut();
+                intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/mobi/utanow/App/Login/LoginActivity.java
+++ app/src/main/java/com/mobi/utanow/App/Login/LoginActivity.java
@@ -1,15 +1,86 @@
 package com.mobi.utanow.App.Login;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.LoginManager;
+import com.facebook.login.widget.LoginButton;
+import com.mobi.utanow.App.Dashboard.DashboardActivity;
+import com.mobi.utanow.myapplication.R;
+
+import java.io.Console;
+import java.util.Arrays;
 
 /**
  * Created by Anthony on 11/13/15.
  */
 public class LoginActivity extends AppCompatActivity {
+
+    LoginButton fbLoginButton;
+    CallbackManager callbackManager;
+    Context context;
+    TextView tv;
+    Intent intent;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
+        context = this;
+
+        // Check if user is already logged in
+        if(AccessToken.getCurrentAccessToken()!=null) {
+            Log.d("fb token", AccessToken.getCurrentAccessToken().getToken());
+            intent = new Intent(context,DashboardActivity.class);
+            startActivity(intent);
+        }
+
+        fbLoginButton = (LoginButton) findViewById(R.id.login_button);
+
+        fbLoginButton.setReadPermissions(Arrays.asList("user_groups"));
+
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //user logged in correctly
+                intent = new Intent(context,DashboardActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                // User closed login window (make sure)
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(context,"an error has occured please try again",Toast.LENGTH_SHORT);
+                Log.e("Loggin Error",exception.getLocalizedMessage());
+            }
+        });
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Call callback manager when login activity is finished
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 
 }
